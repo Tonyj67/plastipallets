3bf6501456f85bc49b814fae12ec33ed5a044915
Tonyj67 <64521001+Tonyj67@users.noreply.github.com>
2025-09-26 13:07:36 +0200

    Clean urls and add sitemap route

--- plastipallets/urls.py
+++ plastipallets/urls.py
@@ -15,46 +15,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # plastipallets/urls.py
+"""
+URL configuration for plastipallets project.
+"""
+
 from django.contrib import admin
 from django.urls import path, include
-#from django.views.generic import TemplateView
-#from django.http import HttpResponse
-#from django.views.generic import RedirectView
-
-#def home_view(request):
-   # return HttpResponse("""
-    #<h1>PlastiPallets is Working!</h1>
-   # <p>Your Django app is successfully deployed on Heroku!</p>
-    #<p><a href="/admin/">Admin Panel</a></p>
-    #""")
+from django.contrib.sitemaps.views import sitemap
+from main.sitemaps import sitemaps_dict   # adjust import if your file name differs
 
 urlpatterns = [
-    path("", include("main.urls")),  # app pages
+    # Admin panel
     path("admin/", admin.site.urls),
-   # path('', TemplateView.as_view(template_name='index.html'), name='home'),
-    #path('', home_view),
-    #path('', RedirectView.as_view(url='https://www.plastipallets.com/', permanent=True)),
+
+    # All site pages live inside the 'main' app
+    path("", include("main.urls")),
+
+    # XML sitemap for search engines
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps_dict},
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
