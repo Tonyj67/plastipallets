fa0bba976c33e5a0624a79e2f10b6e38560cdcab
Tonyj67 <64521001+Tonyj67@users.noreply.github.com>
2025-09-29 08:09:12 +0200

    Fix SSL settings

--- plastipallets/settings.py
+++ plastipallets/settings.py
@@ -149,7 +149,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / "staticfiles"
 os.makedirs(os.path.join(BASE_DIR, 'static'), exist_ok=True)
 
-SECURE_SSL_REDIRECT = False
+SECURE_SSL_REDIRECT = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
