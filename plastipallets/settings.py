5767ffade6c1958ff07bd65593974bb49ccd85f8
Tonyj67 <64521001+Tonyj67@users.noreply.github.com>
2025-09-29 07:41:43 +0200

    Fix middleware path to main.middleware

--- plastipallets/settings.py
+++ plastipallets/settings.py
@@ -61,7 +61,7 @@ INSTALLED_APPS = [
 SITE_ID = 1
 
 MIDDLEWARE = [
-    'pallet_project.middleware.WWWRedirectMiddleware',  # ← ADD THIS LINE
+    'main.middleware.WWWRedirectMiddleware',  # ← ADD THIS LINE
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
